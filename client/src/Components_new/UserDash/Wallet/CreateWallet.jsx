daf6fb06e0b214496d3029e08827a2964527b664
sebas-dev-lab <lb.lescano.vs@gmail.com>
2021-04-23 13:28:34 -0300

    modificado select

--- client/src/Components_new/UserDash/Wallet/CreateWallet.jsx
+++ client/src/Components_new/UserDash/Wallet/CreateWallet.jsx
@@ -5,6 +5,23 @@ import Button from "@material-ui/core/Button";
 import { useDispatch } from "react-redux";
 import { addWallet } from "../../../Redux/Actions/wallet";
 import { useHistory } from "react-router-dom";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import eth from "../../../../node_modules/cryptocurrency-icons/svg/icon/eth.svg";
+import dai from "../../../../node_modules/cryptocurrency-icons/svg/icon/dai.svg";
+
+const cryptos = [
+  {
+    name: "ETH",
+    icon: eth,
+  },
+  {
+    name: "DAI",
+    icon: dai,
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,15 +35,31 @@ const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(2),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
 }));
 
-const AddWallet = () => {
+const AddWallet = ({ handleClose }) => {
   const classes = useStyles();
   let initialState = {
     wallet_coint: "",
     wallet_name: "",
   };
   let [wallet, setWallet] = useState(initialState);
+
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -40,19 +73,35 @@ const AddWallet = () => {
 
   const createWallet = (e) => {
     e.preventDefault();
+    console.log(wallet);
     dispatch(addWallet(wallet.wallet_name, wallet.wallet_coint, history));
     clearState();
+    handleClose();
   };
+
   return (
     <form className={classes.root}>
-      <TextField
-        className={classes.margin}
-        id="custom-css-standard-input"
-        label="Wallet Name"
-        name="wallet_name"
-        value={wallet.wallet_name}
-        onChange={(e) => onChange(e)}
-      />
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          name="wallet_name"
+          value={wallet.wallet_name}
+          onChange={(e) => onChange(e)}
+        >
+          {cryptos.map((cry) => {
+            return (
+              <MenuItem value={cry.name} name="wallet_name">
+                {" "}
+                <img src={cry.icon} />
+                {cry.name}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+
       <TextField
         className={classes.margin}
         id="custom-css-standard-input"
--- client/src/Components_new/UserDash/Wallet/index.jsx
+++ client/src/Components_new/UserDash/Wallet/index.jsx
@@ -4,7 +4,11 @@ import Paper from "@material-ui/core/Paper";
 import DashTable from "./Table";
 import AddWallet from "./CreateWallet";
 import TotalTable from "./TotalTable";
-import { makeStyles } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { useSpring, animated } from "react-spring/web.cjs"; // web.cjs is required for IE 11 support
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -13,10 +17,61 @@ const useStyles = makeStyles((theme) => ({
     overflow: "auto",
     flexDirection: "column",
   },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper_modal: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
 
 const WalletDash = ({ user, coins, fixedHeightPaper }) => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <Grid item xs={12} md={8} lg={9}>
@@ -26,8 +81,25 @@ const WalletDash = ({ user, coins, fixedHeightPaper }) => {
       </Grid>
       <Grid item xs={12} md={4} lg={3}>
         <Paper className={fixedHeightPaper} className={classes.paper}>
-          <h3>Agrega una billetera</h3>
-          <AddWallet />
+          <Button onClick={handleOpen}>Agregar Billetera</Button>
+          <Modal
+            aria-labelledby="spring-modal-title"
+            aria-describedby="spring-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper_modal}>
+                <AddWallet handleClose={handleClose} />
+              </div>
+            </Fade>
+          </Modal>
         </Paper>
         <br></br>
         <Paper className={fixedHeightPaper} className={classes.paper}>
